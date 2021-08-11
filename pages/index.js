91edbc30b0a2d435dee2ff33ad1b7a8958147ae8
dev-owen <jgyuity1289@gmail.com>
2021-08-12 01:29:39 +0900

    feat: install mui, styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { atom, RecoilRoot } from 'recoil';
 import ItemList from '../components/ItemList';
+import Head from 'next/head';
 
 const App = () => {
 	return (
+		<>
+			<Head>
+				<title>Carbon Zero</title>
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+			</Head>
 		<RecoilRoot>
-			<p>recoil</p>
 			<ItemList />
 		</RecoilRoot>
+		</>
 	);
 
 };
