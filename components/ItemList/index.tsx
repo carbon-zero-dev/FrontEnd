4fe5ad6076167b2041c11cb07a96be0a56186363
joielee <2j00923@gmail.com>
2021-08-13 12:01:51 +0900

    feat/ typescript

--- components/ItemList/index.tsx
+++ components/ItemList/index.tsx
@@ -6,7 +6,6 @@ import { itemListSumState } from '../../recoil/selectors';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
-
 const ItemContainer = styled.div`
 	display: flex;
 	padding: 10px;
@@ -18,28 +17,33 @@ const ItemBox = styled.div`
 	margin: 10px;
 `;
 
-
 const ItemList = () => {
-
 	const itemList = useRecoilValue(itemListState);
 	const setItemList = useSetRecoilState(itemListState);
 	const sum = useRecoilValue(itemListSumState);
 
+	const greeting = (name: string) => {
+		console.log('hello ' + name);
+	};
+
 	return (
 		<div>
 			<ItemContainer>
-			{itemList.map(item => {
-				return (
-					<ItemBox>
-						<p>{item.id}</p>
-						<p>{item.name}</p>
-						<p>{item.category}</p>
-						<Button variant="contained">버튼</Button>
-					</ItemBox>
-				);
-			})}
+				{itemList.map(item => {
+					return (
+						<ItemBox key={item.id}>
+							<p>{item.id}</p>
+							<p>{item.name}</p>
+							<p>{item.category}</p>
+							<Button variant="contained">버튼</Button>
+						</ItemBox>
+					);
+				})}
 			</ItemContainer>
 			<p>sum : {sum}</p>
+			<Button variant="contained" onClick={() => greeting('stranger')}>
+				부르기
+			</Button>
 		</div>
 	);
 };
