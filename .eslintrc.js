07b073ade5eec6495ec775258833e068c1d46019
joielee <2j00923@gmail.com>
2021-08-13 09:37:14 +0900

    Revert "feat: add typescript"
    This reverts commit 3867da3abcd969682b7fa7c0b85108f985d5f7af.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true,
-		es2021: true,
-	},
-	extends: [
-		'next',
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'prettier',
-	],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {
-		'@next/next/no-page-custom-font': 'off',
-		'no-unused-vars': 'off',
-	},
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "airbnb-base",
+        "plugin:prettier/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
--- components/ItemList/index.jsx
+++ components/ItemList/index.jsx
@@ -6,18 +6,21 @@ import { itemListSumState } from '../../recoil/selectors';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
 
+
 const ItemContainer = styled.div`
 	display: flex;
 	padding: 10px;
-`;
+`
 
 const ItemBox = styled.div`
 	border: 1px solid red;
 	padding: 10px;
 	margin: 10px;
-`;
+`
+
 
 const ItemList = () => {
+
 	const itemList = useRecoilValue(itemListState);
 	const setItemList = useSetRecoilState(itemListState);
 	const sum = useRecoilValue(itemListSumState);
@@ -25,16 +28,16 @@ const ItemList = () => {
 	return (
 		<div>
 			<ItemContainer>
-				{itemList.map(item => {
-					return (
-						<ItemBox key={item.id}>
-							<p>{item.id}</p>
-							<p>{item.name}</p>
-							<p>{item.category}</p>
-							<Button variant="contained">버튼</Button>
-						</ItemBox>
-					);
-				})}
+			{itemList.map(item => {
+				return (
+					<ItemBox>
+						<p>{item.id}</p>
+						<p>{item.name}</p>
+						<p>{item.category}</p>
+						<Button variant="contained">버튼</Button>
+					</ItemBox>
+				);
+			})}
 			</ItemContainer>
 			<p>sum : {sum}</p>
 		</div>
