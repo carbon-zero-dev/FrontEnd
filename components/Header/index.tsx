2d33872a941ec7d2c22f5ef49f47c3689a92a833
joielee09 <2j00923@gmail.com>
2021-09-03 17:19:19 +0900

    feat: header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,29 +1,52 @@
 // @flow
+
 import * as React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import logo from '../../public/logo.png';
 import styled from 'styled-components';
 
+// theme color: 3C9D46 8DC3A7
 const HeaderContainer = styled.div`
-  width: 100%;
-  height: 80px;
-  background-color: #8DC3A7;
-  color: black;
+	display: flex;
+	flex-direction: column;
+`;
+
+const NavigationBar = styled.div`
+	background-color: #373d3e;
+	display: flex;
+	justify-content: left;
 	padding: 20px;
-	text-align: center;
-	
-	h1 {
-		margin: 0 0 10px;
-	}
-	
-	h2 {
-		margin: 0
-	}
+	padding-left: 100px;
+`;
+const Banner = styled.div`
+	height: 230px;
+	background-color: #3c9d46;
+`;
+
+const NavBtn = styled.a`
+	color: white;
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	margin-left: 60px;
+	cursor: pointer;
+	text-decoration: none;
 `;
 
 const Header = () => {
 	return (
 		<HeaderContainer>
-			<h1>탄소노노</h1>
-			<h2>여러분의 선택으로 지구를 살릴 수 있습니다.</h2>
+			{/* 네비게이션 바 */}
+			<NavigationBar>
+				<Image src={logo} alt="로고" />
+				<NavBtn href="/"> HOME </NavBtn>
+				<NavBtn href="/"> SHOP </NavBtn>
+				<NavBtn href="/"> INFO </NavBtn>
+			</NavigationBar>
+			{/* 배너 */}
+			{/* <Banner></Banner> */}
 		</HeaderContainer>
 	);
 };
